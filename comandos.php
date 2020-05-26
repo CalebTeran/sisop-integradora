61baebb552cb1f92357d25899c1cd9f9e615d82e
Caleb Teran <tic19311037@uthermosillo.edu.mx>
2020-05-25 20:42:43 -0700

    make it better

---
+++ comandos.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integradora</title>
+
+</head>
+<body>
+	<img src="img/shibita.png" alt="shibitahermoso">
+	<h1>Tabla de comandos</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus aut tenetur saepe ut, nemo dolorum. Quasi saepe laborum, pariatur expedita dolores, iste quaerat deleniti, ex ipsa soluta, mollitia quibusdam optio?</p>
+	<a href="index.html"> Volver al Home</a>
+<div id="tablaBox"> <img src="" alt=""> 
+	<table class="tabla" border="3">
+	<caption>Lista de comandos</caption>
+	<thead>
+		<tr>
+			<th>Windows</th>
+			<th>Linux</th>
+			<th>Descripcion</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>cd</td>
+			<td>cd</td>
+			<td>data</td>
+		</tr>
+		<tr>
+			<td>ls</td>
+			<td>ls</td>
+			<td>data</td>
+		</tr>
+		<tr>
+			<td>data</td>
+			<td>data</td>
+			<td>data</td>
+		</tr>
+		<tr>
+			<td>data</td>
+			<td>data</td>
+			<td>data</td>
+		</tr>
+		<tr>
+			<td>data</td>
+			<td>data</td>
+			<td>data</td>
+		</tr>
+		<tr>
+			<td>data</td>
+			<td>data</td>
+			<td>data</td>
+		</tr>
+		<tr>
+			<td>data</td>
+			<td>data</td>
+			<td>data</td>
+		</tr>
+		<tr>	
+			<td>data</td>
+			<td>data</td>
+			<td>data</td>
+		</tr>
+		<tr>
+			<td>data</td>
+			<td>data</td>
+			<td>data</td>
+		</tr>
+		<tr>
+			<td>data</td>
+			<td>data</td>
+			<td>data</td>
+		</tr>
+	</tbody>
+</table>
+
+<a href=""></a>
+</div>
+<div class="box">
+  <h2>Background Colors</h2>
+  <p>Try changing the background <span>colors</span>.</p>
+</div>
+<div class="box2"> <span>HOLA</span></p></div>
+<div class="container"> 
+	<div class="colorBox1">Box1</div>
+	<div class="colorBox2">Box2</div>
+	<div class="colorBox3">Box3</div>
+	<div class="colorBox4">Box4</div>
+	<div class="colorBox5">Box5</div>
+	<div class="colorBox6">Box6</div>
+	<div class="colorBox7">Box7</div>
+	<div class="colorBox8">Box8</div>
+</div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>  
+    <img src="img/shibita.png" alt="shibitahermoso">
+    <h1>Jehú Terán</h1>
+    <a href="comandos.html">Lista de comandos de terminal</a>
+   <div id="tablaBox">
+       
+    <table class="tabla">
+        <tbody><tr>
+            <th>Practicas de sistemas operativas</th>
+            <th>Tabla de comandos</th>
+        </tr>
+
+        <tr>
+            <td><a href="https://ubuntu.com/" target="_blank">Ubuntu</a></td>
+            <td><a href="integrador.html">Lista de comandos</a></td>
+        </tr>
+
+        <tr>
+            <td><a href="https://www.virtualbox.org/" target="_blank">Virtual Box</a></td>
+            <td><a href="https://rufus.ie/" target="_blank">Rufus</a></td>
+        </tr>
+
+    </tbody></table>
+            <br>
+    <table>
+        <tbody><tr>
+            <th>Heroku</th>
+            <th>Mi App heroku</th>
+        </tr>
+
+        <tr>
+            <td><a href="https://www.heroku.com/" target="_blank">Heroku</a></td>
+            <td><a href="https://juanapodaca.herokuapp.com/" target="_blank">Mi proyecto Heroku</a></td>
+            
+        </tr>
+    </tbody></table>
+            <br>
+    <table>
+        <tbody><tr>
+            <th>000WebHost</th>
+            <th>Mi App 000WebHost</th>
+        </tr>
+
+        <tr>
+            <td><a href="https://www.000webhost.com/" target="_blank">000WebHost</a></td>
+            <td><a href="https://jehuteran.000webhostapp.com" target="_blank">Mi proyecto 000WebHost</a></td>
+        </tr>
+    </tbody></table>
+   </div>
+   <button class="btn">BOTON</button> 
+</body>
+</html>
