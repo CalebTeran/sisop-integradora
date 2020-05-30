2b7cfc8d66b014c1bffeffdb26391829e94d0f68
Caleb Teran <tic19311037@uthermosillo.edu.mx>
2020-05-29 18:53:09 -0700

    #3Links

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <style>
+        h1{
+            color: white;
+        }
+        table{
+            text-align: center;
+        }
+    </style>
 	<title></title>
 	<link rel="stylesheet" href="style.css">
 </head>
@@ -15,8 +23,8 @@
    
    <div id="tablaBox">
     <table class="tabla">
+        <h1>Practicas de sistemas operativos</h1>
         <tbody><tr>
-            <th>Practicas de sistemas operativas</th>
             <th>Tabla de comandos</th>
         </tr>
 
@@ -24,20 +32,45 @@
             <td><a href="https://ubuntu.com/" target="_blank">Ubuntu</a></td>
             <td><a href="comandos.php">Lista de comandos</a></td>
         </tr>
-
+    </tbody></table>
+    <h1>Sistemas Operativos</h1>
+            <br>
+    <table>
+        <tbody><tr>
+            <th>Windows</th>
+            <th>Ubuntu</th>
+            <th>Lubuntu</th>
+        </tr>
         <tr>
-            <td><a href="https://www.virtualbox.org/" target="_blank">Virtual Box</a></td>
+            <td>
+            <a href="https://www.microsoft.com/es-mx/windows/" target="_blank">Windows</a>
+            </td>  
+            <td><a href="https://ubuntu.com" target="_blank">Ubuntu</a></td>
+            <td><a href="https://lubuntu.net" target="_blank">Lubuntu</a></td>
+
+        </tr>
+<h1>Servidores Utilizados</h1>
+     </tbody></table> <h1>Herramientas</h1>
+            <br>
+    <table>
+        <tbody><tr>
+            <th>link powershell</th>
+            <th>Visual Studio</th>
+            <th>Visual Studio Code</th>
+            <th>Rufus</th>
+            <th>Virtual Box</th>
+        </tr>
+        <tr>
+            <td><a href="https://docs.microsoft.com/en-us/powershell/" target="_blank">Power Shell</a></td>  <td><a href="https://visualstudio.microsoft.com/es/" target="_blank">VS</a></td>  <td><a href="https://code.visualstudio.com" target="_blank">VSCode</a></td>
+                        <td><a href="https://www.virtualbox.org/" target="_blank">Virtual Box</a></td>
             <td><a href="https://rufus.ie/" target="_blank">Rufus</a></td>
         </tr>
-
-    </tbody></table>
-            <br>
+<h1>Servidores Utilizados</h1>
+     </tbody></table>
     <table>
         <tbody><tr>
             <th>Heroku</th>
             <th>Mi App heroku</th>
-            <th>link powershell</th>
-
         </tr>
 
         <tr>
@@ -57,12 +90,9 @@
             <td><a href="https://www.000webhost.com/" target="_blank">000WebHost</a></td>
             <td><a href="https://jehuteran.000webhostapp.com" target="_blank">Mi proyecto 000WebHost</a></td>
         </tr>
-        <tr>
-            <td><a href="https://docs.microsoft.com/en-us/powershell/" target="_blank">Power Shell</a></td>
-        </tr>
     </tbody></table>
-
-        </tbody></table>    
+        </tbody></table>
+        <h1>Gits</h1>    
     <table>
         <tbody><tr>
             <th>Git Hub</th>
@@ -72,12 +102,64 @@
             
         </tr>
         <tr>
-            <td><a href="https://www.github.com/" target="_blank">000WebHost</a></td>
-            <td><a href="https://www.gitlab.com" target="_blank">Mi proyecto 000WebHost</a></td>
+            <td><a href="https://www.github.com/" target="_blank">GITHUB</a></td>
+            <td><a href="https://www.gitlab.com" target="_blank">GITLAB</a></td>
             <td><a href="https://github.com/CalebTeran" target="_blank">GitHub Personal</a></td>
             <td><a href="https://gitlab.com/CalubSama" target="_blank">GitLab Persona</a></td>
         </tr>     
     </tbody></table>
+<h1> Bases de datos</h1>
+    <table>
+        <tbody><tr>
+            <th>XAMMP</th>
+                        
+        </tr>
+        <tr>
+            <td><a href="https://www.apachefriends.org/es/index.html" target="_blank">Apache</a></td>
+        </tr>     
+    </tbody></table>
+    <table>
+        <tbody><tr>
+            <th>MYSQL</th>
+                        
+        </tr>
+        <tr>
+            <td><a href="https://www.mysql.com" target="_blank">MySql</a></td>
+        </tr>     
+    </tbody></table>
+    <table>
+        <tbody><tr>
+            <th>MariaDB</th>
+                        
+        </tr>
+        <tr>
+            <td><a href="https://mariadb.org" target="_blank">Maria</a></td>
+        </tr>     
+    </tbody></table>
+    <table>
+        <tbody><tr>
+            <th>PHP</th> 
+        </tr>
+        <tr>
+            <td><a href="https://www.php.net/" target="_blank">php</a></td>
+        </tr>     
+    </tbody></table>
+    <table>
+        <tbody><tr>
+            <th>PhpMyAdmin</th> 
+        </tr>
+        <tr>
+            <td><a href="https://www.phpmyadmin.net" target="_blank">PhpMyAdmin</a></td>
+        </tr>     
+    </tbody></table><table>
+        <tbody><tr>
+            <th>HeidiSql</th> 
+        </tr>
+        <tr>
+            <td><a href="https://www.heidisql.com" target="_blank">HeidiSql</a></td>
+        </tr>     
+    </tbody></table>
+    
    </div>
    <button class="btn">BOTON</button> 
 </body>
